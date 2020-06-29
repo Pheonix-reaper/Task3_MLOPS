ebb4f555efdbb23f3651cb8b2e2c22babaa7d029
Asish Patnaik <48721422+Pheonix-reaper@users.noreply.github.com>
2020-06-29 22:07:11 +0530

    Add files via upload

---
+++ check.py
@@ -0,0 +1,10 @@
+file = open('/task3final/Main_Build.py','r')
+key = file.read()				
+
+if 'keras' or 'tensorflow' in key:		
+	if 'Conv2D' or 'Convolution' or 'Dense' in key:				
+		print('CNNcode')
+	else:
+		print('not a CNNcode')
+else:
+	print('Not Deep Learning')
